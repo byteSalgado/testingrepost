ce6755eba83d59be0ceae75c5d67ed601a61edb9
byteSalgado <63315825+byteSalgado@users.noreply.github.com>
2020-04-28 18:56:35 -0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+apt update
+echo "Bienvenido a la instalacion de mi programa"
+echo "espere por favor"
+sleep 5
+apt-get install python*
+chmod +x complementos.py
+echo "instalacion completada"
+python complementos.py
